5c70814e4900dc2ce1962c268d930aa05360401a
Newton1037 <nitinkumar1037nk@gmail.com>
2024-06-18 22:01:17 +0530

    node

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const {chats} = require("./data/data")
 const connectDB = require("./config/db")
 const dotenv = require("dotenv")
 const colors = require("colors")
+const path = require("path")
 const userRoutes = require("./routes/userRoutes")
 const chatRoutes = require("./routes/chatRoutes")
 const messageRoutes = require("./routes/messageRoutes")
@@ -14,9 +15,19 @@ connectDB()
 
 app.use(express.json())  // to accept json data
 
-app.get( "/" , (req,res) => {
-    res.send("API is running")
-})
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  
+    app.get("*", (req, res) =>
+      res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+    );
+  } else {
+    app.get("/", (req, res) => {
+      res.send("API is running..");
+    });
+  }
+
+
 
 app.use("/api/user" , userRoutes)
 app.use("/api/chat" , chatRoutes)
