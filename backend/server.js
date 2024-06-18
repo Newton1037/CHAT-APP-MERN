ee9587fa8d0e2460a67e90b0a5c2e3d08a1aac1b
Newton1037 <nitinkumar1037nk@gmail.com>
2024-06-18 21:04:24 +0530

    build

--- backend/server.js
+++ backend/server.js
@@ -27,4 +27,42 @@ app.use(errorHandler)
 
 PORT = process.env.PORT || 5000
 
-app.listen(`${PORT}` , console.log(`Server started on port ${PORT}`.yellow.bold))
+const server = app.listen(`${PORT}` , console.log(`Server started on port ${PORT}`.yellow.bold))
+
+const io = require("socket.io")(server , {
+    pingTimeout: 60000,
+    cors: {
+        origin: "http://localhost:3000",
+    },  
+})
+
+io.on("connection" , (socket) => {
+    console.log("connected to socket.io")
+
+    socket.on("setup" , (userData) => {
+        socket.join(userData._id)
+        socket.emit("connected")
+    })
+
+    socket.on("chat connect" , (r) => {
+        socket.join(r)
+        console.log("chatId : " + r)
+    })
+
+    socket.on("typing" , (r) => socket.in(r).emit("typing"))
+    socket.on("stop typing" , (r) => socket.in(r).emit("stop typing"))   
+
+    socket.on("New Message" , (NewMessageReceived) => {
+        var chat = NewMessageReceived.chat
+        if(!chat.users) return console.log("users of this chat are undefined")
+        
+        chat.users.forEach((user) => {
+           if(user._id == NewMessageReceived.sender._id) return         
+           socket.in(user._id).emit("message received" , NewMessageReceived)
+        });
+    })
+
+    socket.off("setup" , () => {
+        socket.leave(userData._id)
+    })
+})
