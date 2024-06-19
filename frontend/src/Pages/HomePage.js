b84101ed3ceda0bc1de3e1d2a34fe3ec5d9465b2
Newton1037 <nitinkumar1037nk@gmail.com>
2024-06-19 18:59:14 +0530

    checkagai

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,20 +1,9 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import {Container , Box , Text , Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 import Login from '../components/authentication/Login'
 import Signup from '../components/authentication/Signup'
-import { useNavigate } from 'react-router-dom'
 
 const HomePage = () => {
-
-  // const navigate = useNavigate()
-
-  // useEffect(() => {
-  //   const user = JSON.parse(localStorage.getItem("userInfo"))
-
-  //   if(user){
-  //     navigate("/chats")
-  //   }
-  // } , [navigate])
 
   return (
     <Container maxW="xl" centerContent>
