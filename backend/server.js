a3b6a1c3de8934dadf77e87b9aefabcf4f711c00
Newton1037 <nitinkumar1037nk@gmail.com>
2024-06-20 18:49:08 +0530

    back

--- backend/server.js
+++ backend/server.js
@@ -17,19 +17,19 @@ connectDB()
 app.use(express.json()) 
 app.use(cors())
 
-const __dirname1 = path.resolve();
+// const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname1, "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname1, "/frontend/build")));
   
-    app.get("*", (req, res) =>
-      res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-    );
-  } else {
-    app.get("/", (req, res) => {
-      res.send("API is running..");
-    });
-  }
+//     app.get("*", (req, res) =>
+//       res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+//     );
+//   } else {
+//     app.get("/", (req, res) => {
+//       res.send("API is running..");
+//     });
+//   }
 
 app.use("/api/user" , userRoutes)
 app.use("/api/chat" , chatRoutes)
